552643bb870c8271c7196e507a0c7b154e0b7293
KatyaBel <ekaterina.belyaeva21@mail.ru>
2024-02-27 14:05:13 +0500

    Добавила возможность выбора направления сортировки

--- main.py
+++ main.py
@@ -8,13 +8,15 @@ def main():
     for i in range(n):
         mas.append(randint(1, 99))
     print('Исходный массив:', mas)
+    index = input('Направление сортировки? 1 - по возрастанию/2 - по убыванию: ')
+    index = int(index)
     i = 0
     #до предпоследнего числа
     while i < n-1:
         j = 0
         while j < n-1-i:
             #сравниваем 2 числа
-            if mas[j] > mas[j+1]:
+            if (index == 1 and mas[j] > mas[j+1]) or (index == 2 and mas[j] < mas[j+1]):
                 mas[j], mas[j + 1] = mas[j + 1], mas[j]
             j += 1
         i += 1
